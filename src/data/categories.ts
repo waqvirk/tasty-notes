0a3552c336aeef422e1ba5486d22ca09e2d7e0af
Umi <umiolo1994@gmail.com>
2026-01-20 00:48:56 +0100

    updated categories page, created category cards component

---
+++ src/data/categories.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    name: "Breakfast",
+    image: "/breakfast.jpg",
+    description: "Start your day right with delicious morning meals",
+    recipeCount: 5
+  },
+  {
+    name: "Lunch",
+    image: "/lunch.jpg",
+    description: "Satisfying midday meals to keep you energized",
+    recipeCount: 5
+  },
+  {
+    name: "Dinner",
+    image: "/dinner.jpg",
+    description: "Hearty evening dishes for the perfect end to your day",
+    recipeCount: 6
+  }
+];
+
+export default categories;
